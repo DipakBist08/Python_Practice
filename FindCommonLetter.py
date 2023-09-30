e84778b48939b21838dc946bd0a7906aab38a603
Dipak Bist <mbdds54@gmail.com>
2023-09-30 16:24:14 +0545

    Find the common letter from two strings

---
+++ FindCommonLetter.py
@@ -0,0 +1,17 @@
+#write a program to find common letter from two string.
+#string 1st = NAINA
+#string 2nd =REENA
+#step first remove  duplicates
+def Find_Common_Letter():
+    str1=input('Enter First String : ')
+    str2=input('Enter Second String: ')
+    s1=set(str1) #set will remove duplicate elements
+    s2=set(str2)
+    common_letter=s1 & s2 #& letter will extrect common letter form the string
+    # print(s1) To check output
+    # print(s2) To check output
+    print(common_letter)
+Find_Common_Letter()
+
+
+
