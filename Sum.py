fcfe33b1643c512823237deb7fa52a9968279430
Dipak Bist <mbdds54@gmail.com>
2023-10-01 16:29:06 +0545

    Basic sum of two numbers

---
+++ Sum.py
@@ -0,0 +1,6 @@
+print('Find sum of the two numbers.')
+#if you do not provide datatype then it will  treat user input as a string.
+num1 = int(input('Enter First Number: '))
+num2 = int(input('Enter Second Number'))
+sum =(num1+num2)
+print(sum)
